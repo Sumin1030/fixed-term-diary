d0a9e87476100eee97fa8ae30e4fdc9d3fdf39e0
sumin <gjtnals258@gmail.com>
2024-01-29 13:32:10 -0500

    [기능 구현] 블로그 포스팅 제목 작성 textarea 구현  - 제목이 길어지면 자동으로 textarea height 조절  - Enter은 제목에 포함 안 되도록 막음

--- client/src/component/BlogWrite.js
+++ client/src/component/BlogWrite.js
@@ -1,10 +1,47 @@
 import axios from 'axios';
+import {useRef} from 'react';
 
 function BlogWrite(props) {
 
+    const padding = 10;
+
+    const handleOnKeyPress = (e) => {
+        console.log(e.key, e.target.value);
+    }
+    const postingArea = useRef(null);
+
+    const addTextArea = () => {
+        console.log('addTextArea');
+    }
+
+    const addImage = () => {
+        console.log('addImage');
+    }
+
+    // 제목 작성 시 엔터 입력되지 않도록 함.
+    const checkEnter = (e) => {
+        if(e.key == 'Enter') e.nativeEvent.returnValue = false;
+    }
+
+    // textErea height 조절
+    const setTitleHeight = (e) => {
+        e.target.style.height = 'auto'; //height 초기화
+        e.target.style.height = (e.target.scrollHeight - padding) + 'px';
+    }
+
     return (
         <div className="blog-write">
-            <textarea className='input-text' />
+            <div className="blog-write-area">
+                <div className="blog-write-title">
+                    <textarea className='blog-write-title-input input-text' type='text' placeholder='title' onKeyPress={checkEnter} onChange={setTitleHeight} rows={1}></textarea>
+                </div>
+                <div className="blog-write-content" ref={postingArea}></div>
+            </div>
+            <div className="blog-write-button">
+                <div className="add-textarea" onClick={addTextArea}>TEXT</div>
+                <div className="add-image" onClick={addImage}>IMAGE</div>
+            </div>
+            {/* <textarea className='input-text' onKeyDown={handleOnKeyPress}/> */}
         </div>
     );
 }
